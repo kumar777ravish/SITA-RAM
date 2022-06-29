c4e74ce39fb79834ec45e74d322f7127910a9a60
Ravish Kumar <rvshcistb@gmail.com>
2022-06-29 14:58:17 +0530

    learning c#

---
+++ WHILEEVENINCREMENT/WHILEEVENINCREMENT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHILEEVENINCREMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 10;
+            int x;
+            Console.WriteLine("user should print all even numbers between 10 to 50");
+            x = Convert.ToInt32(Console.ReadLine());
+            while (i <= x)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
